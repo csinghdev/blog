983efb7c5f5f48bc7e8df0dd1d33110eb4a54082
Chandresh <chandresh989@gmail.com>
2015-04-10 01:16:50 -0700

    created header, footer and a css file for layout

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>CodeKamp | Create a new blog</title>
+	<link rel="stylesheet" type="text/css" href="layout.css">
+</head>
+    <div id="header">
+			<img height="50px" width="50px" style="display:inline;" src="http://www.c-tides.iitm.ac.in/images/c-tides.png"/>
+			<h1 style="display:inline;">CodeKamp</h1>
+		</div>
+<body background="http://thecutestblogontheblock.com/wp-content/uploads/2011/11/hilltop-hideaway-free-stretch-blog-background-wood-1000x500.jpg">
--- index.php
+++ index.php
@@ -5,23 +5,23 @@
 	$result = $connection->query($query);
 	$indexOfBlogs=$result->fetch_all(MYSQLI_ASSOC);
 ?>
-<!DOCTYPE html>
-<html>
-<head lang="en">
-	<meta charset="UTF-8">
-	<title>codekamp</title>
-</head>
-<body>
-<h1>List of all blogs</h1>
-<ul>
+<?php require "header.php" ?>
+	<div id="main1">
+		<h1>List of all blogs</h1>
+		<ul>
    <?php
 		foreach($indexOfBlogs as $blog)
 		{
 			echo '<li><a href="view.php?id=' . $blog['id'] . '">' . $blog['title'] . '</a></li>';
 		}
 	?>
-</ul>
-<h2>To add a new post </h2>
-<a href="create.php">Click Here</a>
-</body>
-</html>
+	</ul>
+	</div>
+	<div id="main2">
+		<h2>To add a new post </h2>
+		<a href="create.php">Click Here</a>
+</div>
+<div id="main3">
+	<p>For editing selct the blog then click on edit in the view page and then edit the blog.</p>
+</div>
+<?php require "footer.php" ?>
