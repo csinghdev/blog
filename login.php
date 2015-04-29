726d9e5786307219f91e7bbd139a4b67da06a8b6
Chandresh <chandresh989@gmail.com>
2015-04-29 04:36:55 -0700

    added a working signup page with table in db

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php require "header.php" ?>
+<h1>Sign in</h1>
+<form action="store.php" method="POST">
+<label for="id" >Enter Id : </label>
+	<input id="id" type="integer" name="id" placeholder="Enter id here"/>
+	<br><br>
+  <label for="title" >Enter Title : </label>
+  	<input id="title" type="text" name="title" placeholder="Enter title here"/>
+  	<br><br>
+    <label for="content" >Enter Content : </label>
+    	<textarea id="content" type="text" name="content" placeholder="Enter content here"/></textarea>
+    <input type="submit" style="..." value="Create" >
+</form>
+<?php require "footer.php" ?>
---
+++ signup.php
@@ -0,0 +1,38 @@
+<?php
+if (count($_POST))
+{
+	$name = $_POST['name'];
+  	$username = $_POST['username'];
+  	$email = $_POST['email'];
+  	$password = md5($_POST['password']);
+  	require_once "config.php";
+
+  	$result = "INSERT INTO user (name,username,email,password) VALUES ('$name','$username','$email','$password')";
+  
+  	if ($connection->query($result) === TRUE)
+  	{
+    	$newId = $connection->insert_id;
+    	echo "New User created successfully";
+  	} else
+  	{
+    	echo "Error: " . $result . "<br>" . $connection->error;
+  	}
+ }
+?>
+<?php require "header.php" ?>
+<h1>Sign Up</h1>
+<form action="signup.php" method="POST">
+<label for="name" >Enter Name : </label>
+	<input id="name" type="text" name="name" placeholder="Enter name here"/>
+	<br><br>
+<label for="username" >Enter User Id : </label>
+	<input id="name" type="text" name="username" placeholder="Enter user user name"/>
+	<br><br>
+  <label for="email" >Enter Email : </label>
+  	<input id="email" type="email" name="email" placeholder="Enter email here"/>
+  	<br><br>
+    <label for="password" >Enter Password : </label>
+    	<input id="password" type="password" name="password" placeholder="Enter content here"/>
+    <input type="submit" style="..." value="Create" >
+</form>
+<?php require "footer.php" ?>
